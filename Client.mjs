1c287f389d183c691deb49a2d9ccb4ca2304d812
Diego Carmona Bernal <cbdiegox@gmail.com>
2023-02-01 03:21:02 -0600

    Add config Client

--- Client.mjs
+++ Client.mjs
@@ -1,28 +1,35 @@
 import net from 'net'
 
+//  Configuración del servidor
 const port = 3000
-const host = "127.0.0.1"
-
+const host = "192.168.1.65"
+//  Socket que se conecta con el servidor
 const client = new net.Socket()
 
+//  Realizamos la conexión
 client.connect(port, host)
 
-client.on('data', (data) => {
-    console.log(data.toString().trim())
-})
-
+//  Se ejecuta solo si la conexión fue exitosa con el servidor
 client.on('connect', () => {
     process.stdin.on('data', (data) => {
+        //  Se convierte el mensaje de Buffer a String
         client.write(data.toString().trim())
     })
-
+})
+//  Se ejecuta cuando recibe información
+client.on('data', (data) => {
+    //  Se convierte el mensaje de Buffer a String
+    let message = data.toString().trim()
+    console.log(message)
 })
 
-client.on('error', (err) => {
-    if (err.errno == -4077) {
-        console.log("Se ha perdido comunicación con el servidor")
-    } else {
-        console.error(err)
-    }
+//  Con esta función finalizamos el programa.
+client.on('close', () => {
+    console.log("Sesión finalizada")
     process.exit(0)
+})
+
+//  Evitamos errores
+client.on('error', (err) => {
+    console.log("Error: ¡Ups! Parece que hay un problema con su conexión")
 })
